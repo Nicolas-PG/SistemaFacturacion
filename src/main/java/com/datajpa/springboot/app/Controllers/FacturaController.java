fc5a3750bf39fdee3e825537d6cc8d61fb63f0c5
Nicolas Pulti <nicopultigisondo@gmail.com>
2023-07-07 19:22:59 -0300

    Se agrego crear factura

--- src/main/java/com/datajpa/springboot/app/Controllers/FacturaController.java
+++ src/main/java/com/datajpa/springboot/app/Controllers/FacturaController.java
@@ -2,11 +2,15 @@ package com.datajpa.springboot.app.Controllers;
 
 import com.datajpa.springboot.app.Entity.Cliente;
 import com.datajpa.springboot.app.Entity.Factura;
+import com.datajpa.springboot.app.Entity.ItemFactura;
 import com.datajpa.springboot.app.Entity.Producto;
 import com.datajpa.springboot.app.Service.IClienteService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
@@ -17,6 +21,7 @@ import java.util.Map;
 @SessionAttributes("factura")
 public class FacturaController {
 
+    private final Logger log = LoggerFactory.getLogger(getClass());
     @Autowired
     private IClienteService clienteService;
 
@@ -43,5 +48,27 @@ public class FacturaController {
     public @ResponseBody  List<Producto> cargarProductos(@PathVariable String term){   // el ResponseBody suprime las vista de thymeleaf
 
         return clienteService.findByNombre(term);
+    }
+
+    @PostMapping("/form")
+    public String guardar(Factura factura, @RequestParam(name = "item_id[]", required = false) Long[] itemId,
+                          @RequestParam(name = "cantidad[]", required = false) Integer[] cantidad, RedirectAttributes flash, SessionStatus status){
+
+        for (int i=0; i< itemId.length; i++){
+            Producto producto = clienteService.findProductoById(itemId[i]);
+
+            ItemFactura linea = new ItemFactura();
+            linea.setCantidad(cantidad[i]);
+            linea.setProducto(producto);
+
+            factura.addItemFactura(linea);
+
+            log.info("ID: " + itemId[i].toString() + ", cantidad: " + cantidad[i].toString());
+        }
+
+        clienteService.saveFactura(factura);
+        status.setComplete();
+        flash.addFlashAttribute("success","Factura creada con exito!");
+        return "redirect:/ver/" +factura.getCliente().getId();
     }
 }
